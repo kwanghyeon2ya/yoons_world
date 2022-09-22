1d9616e900684a3f952266833c56258326f3e017
truth286 <truth286@25af98ad-8d66-45ff-b3f8-76667b6dbbd7>
2022-09-22 23:01:49 +0000

    logger 예제 추가

--- trunk/src/main/java/com/iyoons/world/controller/DemoController.java
+++ trunk/src/main/java/com/iyoons/world/controller/DemoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,10 +23,14 @@ public class DemoController {
 	@Autowired
 	DemoService demoService;
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/getDemoList")
 	public String index(Model model, HttpSession session) {
 		
 		List<DemoVO> list = demoService.getDemoList();
+		
+		logger.info("list size : "+list.size());
 		
 		model.addAttribute("list", list);
 		return "demo/getDemoList";
