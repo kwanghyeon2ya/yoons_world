654d7e5146dcf564f633175f913bc46a0d123a4d
sohee <sohee@25af98ad-8d66-45ff-b3f8-76667b6dbbd7>
2022-09-28 06:42:36 +0000

    UserServiceImpl 추가

---
+++ trunk/src/main/java/com/iyoons/world/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.iyoons.world.service.impl;
+
+import com.iyoons.world.service.UserService;
+import com.iyoons.world.vo.UserVO;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserVO getUser(UserVO userVO) throws Exception {
+		// TODO Auto-generated method stubㄴ
+		return null;
+	}
+
+}
